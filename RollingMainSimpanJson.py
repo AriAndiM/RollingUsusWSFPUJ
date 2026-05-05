297bc4fbcc4226068ebab71dd2b4c606b9a437bf
Ari Andi Mustofa <83968034+AriAndiM@users.noreply.github.com>
2026-05-05 11:21:19 +0700

    Menampilkan * di tunggu pembayaran.py

--- RollingMainSimpanJson.py
+++ RollingMainSimpanJson.py
@@ -146,18 +146,18 @@ if selection == "Wahana Sejahtera Foods" and selection != "--Pilih Perusahaan--"
                         # 🔥 TAMBAHAN LOGIC LABEL
                         # =========================
                         for line, entries in data.items():
-                        st.markdown(f"**Line - {line}**")
-                        for key, value in entries.items():
-                    
-                            clean_value = value.replace(" *(Tunggu Pembayaran)*", "")
-                    
-                            if clean_value in selected_nama:
-                                value = f"{clean_value} *(Tunggu Pembayaran)*"
-                    
-                            # 🔥 pakai markdown supaya bintang tampil
-                            st.markdown(f"{key}. {value}")
-                    
-                            data[line][key] = value
+                            st.markdown(f"**Line - {line}**")
+                            for key, value in entries.items():
+                        
+                                clean_value = value.replace(" *(Tunggu Pembayaran)*", "")
+                        
+                                if clean_value in selected_nama:
+                                    value = f"{clean_value} *(Tunggu Pembayaran)*"
+                        
+                                # 🔥 pakai markdown supaya bintang tampil
+                                st.markdown(f"{key}. {value}")
+                        
+                                data[line][key] = value
 
                         history[target_date] = data
 
